6ef71fc0cdaf2516637f005d078b4277232f2c8a
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-02 00:27:08 +0100

    wip

--- core/renders_conf.py
+++ core/renders_conf.py
@@ -4,30 +4,27 @@ import logging
 logger = logging.getLogger(__name__)
 
 def render_table(table, template_name="editor/table.html"):
-    return render_to_string(template_name, table)
+    return render_to_string(template_name, {'table': table})
 
 def render_list(list_tool, template_name="editor/list.html"):
-    return render_to_string(template_name, list_tool)
+    return render_to_string(template_name, {'list_tool': list_tool})
 
 
 def render_header(head, template_name="editor/head.html"):
-    logger.info(f"rendering header : {head}")
-    header = render_to_string(template_name, head)
-    logger.info(f"rendering header string: {header}")
-    return header
+    return render_to_string(template_name, {'head': head})
 
 def render_checklist(checklist, template_name="editor/checklist.html"):
-    return render_to_string(template_name, checklist)
+    return render_to_string(template_name, {'checklist:': checklist})
 
 def render_paragraph(paragraph, template_name="editor/paragraph.html"):
     logger.info(f"rendering paragraph : {paragraph}")
-    return render_to_string(template_name, paragraph)
+    return render_to_string(template_name, {'paragraph': paragraph})
 
 def render_quote(quote, template_name="editor/quote.html"):
-    return render_to_string(template_name, quote)
+    return render_to_string(template_name, {'quote': quote})
 
 def render_linktool(linktool, template_name="editor/linktool.html"):
-    return render_to_string(template_name, linktool)
+    return render_to_string(template_name, {'linktool': linktool})
 
 
 BLOCK_MAPPING = {
