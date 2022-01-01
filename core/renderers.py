01a9aa262de1ae3d2ad82eedd7fbba334aea0980
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-02 00:31:45 +0100

    wip

--- core/renderers.py
+++ core/renderers.py
@@ -1,9 +1,9 @@
 from core.renders_conf import BLOCK_MAPPING
-
+from django.utils.safestring import mark_safe
 
 def render_post(post_dict):
     html_blocks = [BLOCK_MAPPING[block.get('type')](block) for block in post_dict.get('blocks')]
-    return "".join(html_blocks)
+    return mark_safe("".join(html_blocks))
 
     
 
