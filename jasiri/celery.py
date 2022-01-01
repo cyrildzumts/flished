7bce9cff7f0da5803c891c006a5bdca535ffb153
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-01 06:01:09 +0100

    wip

---
+++ jasiri/celery.py
@@ -0,0 +1,20 @@
+
+import os
+from kombu import Exchange, Queue
+from jasiri import settings
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jasiri.settings')
+app = Celery(settings.SITE_NAME)
+app.config_from_object('django.conf:settings', namespace=settings.CELERY_NAMESPACE)
+app.conf.task_queues = (
+    Queue(settings.CELERY_DEFAULT_QUEUE, Exchange(settings.CELERY_DEFAULT_EXCHANGE), routing_key=settings.CELERY_DEFAULT_ROUTING_KEY),
+    Queue(settings.CELERY_OUTGOING_MAIL_QUEUE, Exchange(settings.CELERY_OUTGOING_MAIL_EXCHANGE), routing_key=settings.CELERY_OUTGOING_MAIL_ROUTING_KEY),
+    Queue(settings.CELERY_IDENTIFICATION_QUEUE, Exchange(settings.CELERY_IDENTIFICATION_EXCHANGE), routing_key=settings.CELERY_IDENTIFICATION_ROUTING_KEY),
+)
+app.conf.task_default_queue = settings.CELERY_DEFAULT_QUEUE
+app.conf.task_default_exchange_type = settings.CELERY_DEFAULT_EXCHANGE_TYPE
+app.conf.task_default_routing_key = settings.CELERY_DEFAULT_ROUTING_KEY
+#app.conf.beat_schedule = {}
+app.autodiscover_tasks()
