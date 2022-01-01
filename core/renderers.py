941270b4397cdc715c3d5cf5e0a9c2b1e543200e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-02 00:38:51 +0100

    wip

--- core/renderers.py
+++ core/renderers.py
@@ -2,7 +2,7 @@ from core.renders_conf import BLOCK_MAPPING
 from django.utils.safestring import mark_safe
 
 def render_post(post_dict):
-    html_blocks = [BLOCK_MAPPING[block.get('type')](block).replace("\\n","<br />\\n") for block in post_dict.get('blocks')]
+    html_blocks = [BLOCK_MAPPING[block.get('type')](block).replace("\n","<br />\n") for block in post_dict.get('blocks')]
     return mark_safe("".join(html_blocks))
 
     
