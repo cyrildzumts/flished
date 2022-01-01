8a00632751cac7a2a721e3a7409ebe834882e394
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-02 00:49:25 +0100

    wip

---
+++ blog/tempatetags/blog_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from django.utils.translation import gettext_lazy as _
+from blog import constants as Constants
+from jasiri import utils
+import logging
+
+logger = logging.getLogger(__name__)
+register = template.Library()
+
+@register.filter
+def post_status_value(key):
+    k,v = utils.find_element_by_key_in_tuples(key, Constants.POST_STATUS)
+    if v is None:
+        logger.info(f"post_status_value : Could not found value  for key \"{key}\"")
+        return key
+    return v
