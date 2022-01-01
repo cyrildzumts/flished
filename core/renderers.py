372bdbe871242dee292ab0f83a4d8de2ddc1294c
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-02 00:08:45 +0100

    wip

--- core/renderers.py
+++ core/renderers.py
@@ -2,7 +2,7 @@ from core.renders_conf import BLOCK_MAPPING
 
 
 def render_post(post_dict):
-    html_blocks = [BLOCK_MAPPING[block.get('type')]().replace("\\n","<br />\\n") for block in post_dict.get('blocks')]
+    html_blocks = [BLOCK_MAPPING[block.get('type')](block).replace("\\n","<br />\\n") for block in post_dict.get('blocks')]
     return "".join(html_blocks)
 
     
