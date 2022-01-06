e716907106daddd71f4cbb8ea66a5b52f0ad77cb
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-06 11:45:33 +0100

    wip

--- flished/wsgi.py
+++ flished/wsgi.py
@@ -14,6 +14,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.info("running wsgi.py file")
+print("running wsgi.py file")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flished.settings')
 logger.info("setting default settings module from wsgi.py file")
 application = get_wsgi_application()
