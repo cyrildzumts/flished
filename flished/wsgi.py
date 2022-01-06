c50671b264bcaffec5c55408e945ae72e63ed68a
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-06 11:32:07 +0100

    wip

--- flished/wsgi.py
+++ flished/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import logging
 
+logger = logging.getLogger(__name__)
+logger.info("running wsgi.py file")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flished.settings')
-
+logger.info("setting default settings module from wsgi.py file")
 application = get_wsgi_application()
