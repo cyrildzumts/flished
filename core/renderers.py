feb3eec21f072d0cf978bb6bbf529eaf48d8e21f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-01-12 17:40:45 +0100

    wip

--- core/renderers.py
+++ core/renderers.py
@@ -2,7 +2,7 @@ from core.renders_conf import BLOCK_MAPPING, SUMMARY_TAG
 from django.utils.safestring import mark_safe
 
 def render_post(post_dict):
-    html_blocks = [BLOCK_MAPPING[block.get('type')](block).replace("\n","<br />\n") for block in post_dict.get('blocks')]
+    html_blocks = [BLOCK_MAPPING[block.get('type')](block) for block in post_dict.get('blocks')]
     return mark_safe("".join(html_blocks))
 
 
@@ -13,7 +13,7 @@ def render_summary(post_dict):
             paragraph = block
             break
     if paragraph is not None:
-        return mark_safe(BLOCK_MAPPING[paragraph.get('type')](paragraph).replace("\n","<br />\n"))
+        return mark_safe(BLOCK_MAPPING[paragraph.get('type')](paragraph))
     return ""
 
     
