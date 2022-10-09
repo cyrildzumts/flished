279ccfd8ebbc1bb861fc29124512128c2adc328a
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-10-10 00:15:27 +0200

    wip

--- blog/urls.py
+++ blog/urls.py
@@ -14,7 +14,7 @@ blog_routes = [
     path('delete/<slug:post_slug>/', views.delete_post, name="delete-post"),
     path('update-post/<slug:post_slug>/', views.update_post, name="update-post"),
     path('create-comment/<slug:author>/<slug:post_slug>/', views.create_comment, name="create-comment"),
-    path('categories/<slug:slug>/', views.update_post, name="category-detail"),
+    path('categories/<slug:slug>/', views.category_detail_slug, name="category-detail"),
     path('<slug:author>/', views.author_stories, name="author-stories"),
     path('<slug:author>/<slug:post_slug>/', views.blog_post, name="blog-post"),
 ]
