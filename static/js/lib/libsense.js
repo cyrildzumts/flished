b118c8f5b5e7137f8f8c8e15cc946a31f13c3b30
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2022-10-26 22:19:34 +0200

    update adsense

---
+++ static/js/lib/libsense.js
@@ -0,0 +1,16 @@
+const AD_CLIENT = "ca-pub-7624615584108748";
+const AD_SLOT = "6056470096";
+const AD_FORMAT = "auto";
+const data = [
+    {
+        'ad-format': 'auto','ad-client': "ca-pub-7624615584108748","ad-slot": "6056470096"
+    },
+    {
+        'ad-format': 'autorelaxed','ad-client': "ca-pub-7624615584108748","ad-slot": "5865333179"
+    }
+];
+
+window.addEventListener('load',(event) =>{
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    console.log("loaded adsense");
+});
