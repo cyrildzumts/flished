6254aa96fff88c2c03fda24b12eb58a2f30e0b59
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 13:16:35 +0200

    added backends for mailing

---
+++ core/mail/backends.py
@@ -0,0 +1,19 @@
+import ssl
+from django.utils.functional import cached_property
+from django.core.mail.backends.smtp import EmailBackend as DefaultEmailBackend
+
+
+
+class EMailBackend(DefaultEmailBackend):
+    
+    @cached_property
+    def ssl_context(self):
+        if self.ssl_certfile or self.ssl_keyfile:
+            ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+            ssl_context.load_cert_chain(self.ssl_certfile, self.ssl_keyfile)
+            return ssl_context
+        else:
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            return ssl_context
